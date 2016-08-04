26fa18660de3f774d836988d4201c40a3a81c7b1
Buddy Gorven <buddygorven@msn.com>
2016-08-04 22:51:43 +1200

    Extracted JNI config, removed vagrant.

--- provisioning/linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo add-apt-repository -y ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk g++-multilib
--- provisioning/windows.cmd
+++
@@ -1,5 +0,0 @@
-# https://gist.github.com/gregsochanik/7582661
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-set PATH=%PATH%;%systemdrive%\chocolatey\bin
-choco install jdk8,visualstudiocommunity2013 -y
-if not exist "C:\Program Files\OpenSSH\vagrant" mklink /D "C:\Program Files\OpenSSH\vagrant" "C:\vagrant"
